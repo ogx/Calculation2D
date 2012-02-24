f975a76fdec292caec53b7352e8262f33f6a1b51
malleor <malleor@gmail.com>
2012-02-24 23:01:25 +0100

    little fix after merge

--- proj/calc2d/mmCalculationServer.cpp
+++ proj/calc2d/mmCalculationServer.cpp
@@ -1,5 +1,5 @@
 #include "mmCalculationServer.h"
-#include <mmInterfaceInitializers.h>
+#include <interfaces\mmInterfaceInitializers.h>
 #include <images\mmImagesCalculationManagement.h>
 #include <images\mmImagesCalculationMethodContainerWindows.h>
 #include <memory>
