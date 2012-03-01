29b0b67d7e5f60aa6e8f7aa3a7e6b78a07d76cc9
carasuca <carasuca@gmail.com>
2012-03-01 18:01:17 +0100

    themoob...

---
+++ include/interfaces/mmICalculationMethodContainer.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma message("themoob is a smeghead")
+#include <map>
+#include <vector>
+#include <interfaces/mmIImages.h>
+
+namespace mmImages {
+	class mmImagesCalculationMethodContainerI {
+	public:
+
+		struct
+			sCalculationMethodParams {};
+	};
+}
